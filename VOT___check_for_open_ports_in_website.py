24e8a1befd1a1d0a7e6abe612b81b67db424c962
nadya <107633042+nadenica@users.noreply.github.com>
2023-04-16 00:33:44 +0300

    Update VOT___check_for_open_ports_in_website.py

--- VOT___check_for_open_ports_in_website.py
+++ VOT___check_for_open_ports_in_website.py
@@ -1,28 +1,42 @@
 import socket
 
-
 def check_ports(website):
-
-    website=website[8:]
-    website=website[:-1]
+    # Function to check which ports are open on a given website
+    website = website[8:]
+    website = website[:-1]
     open_ports = []
 
-    for port in range(1024):
+    for port in range(450):
         try:
-        # Create a socket object
+            # Create a socket object
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # Set a timeout of 1 second
+            # Set a timeout of 1 second
             s.settimeout(1)
 
-        # Try to connect to the website on the current port
+            # Try to connect to the website on the current port
             s.connect((website, port))
 
-        # If the connection succeeds, print a message
+            # If the connection succeeds, add the port to the list of open ports
             open_ports.append(port)
 
+        except Exception as e:
+           pass
+
         finally:
-        # Close the socket
+            # Close the socket
             s.close()
 
     return open_ports
+
+def main():
+    # Main function to run the check_ports() function
+    website = "https://www.google.com/"
+    open_ports = check_ports(website)
+    if len(open_ports) > 0:
+        print(f"Open ports: {open_ports}")
+    else:
+        print("No open ports found.")
+
+if __name__ == "__main__":
+    main()
